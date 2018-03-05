2bedb049f40823e78108cec4aa0c4790d47414b1
raacker <haven.cpp@gmail.com>
2018-03-05 12:25:36 +0900

    finish chapter 2

--- 02-hello-world/test.js
+++ 02-hello-world/test.js
@@ -8,7 +8,7 @@ describe('Hello World', function () {
     request(app.listen())
     .get('/')
     .expect(200)
-    .expect('Content-Length', 11)
+    .expect('Content-Length', '11')
     .expect('Content-Type', 'text/plain; charset=utf-8')
     .end(done)
   })
